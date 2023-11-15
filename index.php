d66fe979f6f260498d9b7f767445041dab536eb8
challa saiprakash reddy <112607563+Saiprakash330@users.noreply.github.com>
2023-11-15 14:06:17 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,96 +1,81 @@
-<?php include 'includes/session.php'; ?>
-<?php include 'includes/header.php'; ?>
-<body class="hold-transition skin-blue layout-top-nav">
-<div class="wrapper">
+<!--
+=========================================================
+* Material Kit 2 - v3.0.2
+=========================================================
 
-	<?php include 'includes/navbar.php'; ?>
-	 
-	  <div class="content-wrapper">
-	    <div class="container">
+* Product Page:  https://www.creative-tim.com/product/material-kit 
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Coded by www.creative-tim.com
 
-	      <!-- Main content -->
-	      <section class="content">
-	        <div class="row">
-	        	<div class="col-sm-9">
-	        		<?php
-	        			if(isset($_SESSION['error'])){
-	        				echo "
-	        					<div class='alert alert-danger'>
-	        						".$_SESSION['error']."
-	        					</div>
-	        				";
-	        				unset($_SESSION['error']);
-	        			}
-	        		?>
-	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		                <ol class="carousel-indicators">
-		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-		                </ol>
-		                <div class="carousel-inner">
-		                  <div class="item active">
-		                    <img src="images/banner1.png" alt="First slide">
-		                  </div>
-		                </div>
-		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-		                  <span class="fa fa-angle-left"></span>
-		                </a>
-		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-		                  <span class="fa fa-angle-right"></span>
-		                </a>
-		            </div>
-		            <h2>Monthly Top Sellers</h2>
-		       		<?php
-		       			$month = date('m');
-		       			$conn = $pdo->open();
+ =========================================================
 
-		       			try{
-		       			 	$inc = 3;	
-						    $stmt = $conn->prepare("SELECT *, SUM(quantity) AS total_qty FROM details LEFT JOIN sales ON sales.id=details.sales_id LEFT JOIN products ON products.id=details.product_id WHERE MONTH(sales_date) = '$month' GROUP BY details.product_id ORDER BY total_qty DESC LIMIT 6");
-						    $stmt->execute();
-						    foreach ($stmt as $row) {
-						    	$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
-						    	$inc = ($inc == 3) ? 1 : $inc + 1;
-	       						if($inc == 1) echo "<div class='row'>";
-	       						echo "
-	       							<div class='col-sm-4'>
-	       								<div class='box box-solid'>
-		       								<div class='box-body prod-body'>
-		       									<img src='".$image."' width='100%' height='230px' class='thumbnail'>
-		       									<h5><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
-		       								</div>
-		       								<div class='box-footer'>
-		       									<b>&#36; ".number_format($row['price'], 2)."</b>
-		       								</div>
-	       								</div>
-	       							</div>
-	       						";
-	       						if($inc == 3) echo "</div>";
-						    }
-						    if($inc == 1) echo "<div class='col-sm-4'></div><div class='col-sm-4'></div></div>"; 
-							if($inc == 2) echo "<div class='col-sm-4'></div></div>";
-						}
-						catch(PDOException $e){
-							echo "There is some problem in connection: " . $e->getMessage();
-						}
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
 
-						$pdo->close();
+<!--
+=========================================================
+* PDF Generator using TCPDF 
+=========================================================
 
-		       		?> 
-	        	</div>
-	        	<div class="col-sm-3">
-	        		<?php include 'includes/sidebar.php'; ?>
-	        	</div>
-	        </div>
-	      </section>
-	     
-	    </div>
-	  </div>
-  
-  	<?php include 'includes/footer.php'; ?>
-</div>
+* Coded by oretnom23@gmail.com
+ =========================================================
+-->
+<?php 
+require_once('config.php');
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+$page_name = explode("/",$page)[count(explode("/",$page)) -1];
+?>
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
 
-<?php include 'includes/scripts.php'; ?>
+<?php include_once('includes/header.php') ?>
+
+<body class="index-page bg-gray-200">
+    <script>start_loader()</script>
+    <?php include_once('includes/top-navigation.php') ?>
+    <header class="header-2">
+        <div class="page-header min-vh-45 relative" style="background-image: url('<?= $_settings->info('cover') ?>')">
+            <span class="mask bg-gradient-dark opacity-4"></span>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 text-center mx-auto">
+                        <h1 class="text-white pt-3 mt-n5"><?= $_settings->info('name') ?></h1>
+                        <p class="lead text-white mt-3"><?= ucwords(str_replace(["_","/"]," ",$page)). " Page" ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+        <?php 
+        if($_settings->chk_flashdata('success')):
+        ?>
+        <div class="alert alert-success ?> rounded-0 text-light py-1 px-4 mx-3">
+            <div class="d-flex w-100 align-items-center">
+                <div class="col-10">
+                    <?= $_settings->flashdata('success') ?>
+                </div>
+                <div class="col-2 text-end">
+                    <button class="btn m-0 text-sm" type="button" onclick="$(this).closest('.alert').remove()"><i class="material-icons mb-0">close</i></button>
+                </div>
+            </div> 
+        </div>
+        <?php endif; ?>
+        <?php
+        if(is_file($page.'.php')){
+            include $page.'.php';
+        }else{
+            if(is_dir($page) && is_file($page.'/index.php')){
+                include $page.'/index.php';
+            }else{
+                echo '<h4 class="text-center fw-bolder">Page Not Found</h4>';
+            }
+        }
+        ?>
+    </div>
+
+
+    <?php include_once('includes/footer.php') ?>
+
 </body>
+
 </html>
